6aaea47573a7db5afe48261e77054c6d3caf9aae
Sajay <sajayprakash7@protonmail.com>
2022-07-09 10:43:07 +0530

    update location coordinates

--- config.js
+++ config.js
@@ -20,9 +20,9 @@ const CONFIG = {
 	weatherUnit: 'C', // 'F', 'C'
 	language: 'en', // More languages in https://openweathermap.org/current#multi
 
-	trackLocation: true, // If false or an error occurs, the app will use the lat/lon below
-	defaultLatitude: '37.775',
-	defaultLongitude: '-122.419',
+	trackLocation: false, // If false or an error occurs, the app will use the lat/lon below
+	defaultLatitude: '13.067439', 
+	defaultLongitude: '80.237617',
 
 	// Autochange
 	autoChangeTheme: true,
